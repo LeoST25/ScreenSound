1e926f1435d1072cfdf6db293adc4ba7ea1a152c
LeoST25 <matheusleonardo224@gmail.com>
2023-11-24 20:38:24 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,29 @@
+﻿class Musica
+{
+    public string nome;
+    public string genero;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome da música: {nome}");
+        Console.WriteLine($"Genero: {genero}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        } else
+        {
+            Console.WriteLine("Adquirida o plano Plus+");
+        }
+    }
+
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+        
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,22 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxanne";
+musica1.genero = "Rock";
+musica1.artista = "The Police";
+musica1.duracao = 187;
+musica1.disponivel = true;
+
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica1.genero = "Heavy Metal";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+Console.WriteLine("\n");
+musica2.ExibirFichaTecnica();
+Console.WriteLine("\n");
+musica1.ExibirNomeEArtista();
+Console.WriteLine("\n");
+musica2.ExibirNomeEArtista();
